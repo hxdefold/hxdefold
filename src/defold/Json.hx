550a238aecfa33f840f5f76d38c9ff1f8d10cfea
Dan Korostelev <nadako@gmail.com>
2016-06-06 16:49:11 +0300

    minor

--- src/defold/Json.hx
+++ src/defold/Json.hx
@@ -2,5 +2,5 @@ package defold;
 
 @:native("_G.json")
 extern class Json {
-    static function decode(json:String):lua.Table.AnyTable;
+    static function decode(json:String):lua.Table<String,Dynamic>;
 }
