4d263b24640707eed899d2e3cfef0b97c068c561
Dan Korostelev <nadako@gmail.com>
2016-06-06 17:58:34 +0300

    refine crash externs

--- src/defold/Crash.hx
+++ src/defold/Crash.hx
@@ -2,11 +2,11 @@ package defold;
 
 @:native("_G.crash")
 extern class Crash {
-    static function get_backtrace(handle:CrashHandle):lua.Table.AnyTable; // TODO: figure out what is it
+    static function get_backtrace(handle:CrashHandle):lua.Table<Int,String>;
     static function get_extra_data(handle:CrashHandle):String;
-    static function get_modules(handle:CrashHandle):lua.Table.AnyTable;
+    static function get_modules(handle:CrashHandle):lua.Table.AnyTable; // TODO: what's this table?
     static function get_signum(handle:CrashHandle):Int;
-    static function get_sys_field(handle:CrashHandle, index:Int):String;
+    static function get_sys_field(handle:CrashHandle, index:CrashSysField):String;
     static function get_user_field(handle:CrashHandle, index:Int):String;
     static function load_previous():Null<CrashHandle>;
     static function release(handle:CrashHandle):Void;
@@ -14,16 +14,20 @@ extern class Crash {
     static function set_user_field(index:Int, value:String):Void;
     static function write_dump():Void;
 
-    static var SYSFIELD_ANDROID_BUILD_FINGERPRINT(default,null):Int;
-    static var SYSFIELD_DEVICE_LANGUAGE(default,null):Int;
-    static var SYSFIELD_DEVICE_MANUFACTURER(default,null):Int;
-    static var SYSFIELD_DEVICE_MODEL(default,null):Int;
-    static var SYSFIELD_ENGINE_HASH(default,null):Int;
-    static var SYSFIELD_ENGINE_VERSION(default,null):Int;
-    static var SYSFIELD_LANGUAGE(default,null):Int;
-    static var SYSFIELD_SYSTEM_NAME(default,null):Int;
-    static var SYSFIELD_SYSTEM_VERSION(default,null):Int;
-    static var SYSFIELD_TERRITORY(default,null):Int;
 }
 
 typedef CrashHandle = Int
+
+@:native("_G.crash")
+@:enum extern abstract CrashSysField(Int) {
+    var SYSFIELD_ANDROID_BUILD_FINGERPRINT;
+    var SYSFIELD_DEVICE_LANGUAGE;
+    var SYSFIELD_DEVICE_MANUFACTURER;
+    var SYSFIELD_DEVICE_MODEL;
+    var SYSFIELD_ENGINE_HASH;
+    var SYSFIELD_ENGINE_VERSION;
+    var SYSFIELD_LANGUAGE;
+    var SYSFIELD_SYSTEM_NAME;
+    var SYSFIELD_SYSTEM_VERSION;
+    var SYSFIELD_TERRITORY;
+}
