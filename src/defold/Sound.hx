2ef4285d6746bd560d687c34233a0e7097cbedb1
Dan Korostelev <nadako@gmail.com>
2016-06-06 21:33:50 +0300

    avoid multi-return by boxing them in a table

--- src/defold/Sound.hx
+++ src/defold/Sound.hx
@@ -6,9 +6,20 @@ import defold.support.HashOrString;
 extern class Sound {
     static function get_group_gain(group:HashOrString):Float;
     static function get_group_name(group:HashOrString):String;
-    static function get_groups():lua.Table<Int,String>;
-    static function get_peak(group:HashOrString, window:Float):Dynamic; // TODO: multi-return...
-    static function get_rms(group:HashOrString, window:Float):Dynamic; // TODO: multi-return...
+    static function get_groups():lua.Table<Int,Hash>;
+    inline static function get_peak(group:HashOrString, window:Float):SoundLeftRight<Float> {
+        return untyped __lua__("{{ {0}.get_peak({1}, {2}) }}", Sound, group, window);
+    }
+    inline static function get_rms(group:HashOrString, window:Float):SoundLeftRight<Float> {
+        return untyped __lua__("{{ {0}.get_rms({1}, {2}) }}", Sound, group, window);
+    }
     static function is_music_playing():Bool;
     static function set_group_gain(group:HashOrString, gain:Float):Bool;
+}
+
+abstract SoundLeftRight<T>(lua.Table<Int,T>) {
+    public var left(get,never):T;
+    public var right(get,never):T;
+    inline function get_left() return this[1];
+    inline function get_right() return this[2];
 }
