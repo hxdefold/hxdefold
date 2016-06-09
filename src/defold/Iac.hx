a683a7530925a352a6755a40ed53d772a26a8d6c
Dan Korostelev <nadako@gmail.com>
2016-06-09 22:37:54 +0300

    Document IAC api.

--- src/defold/Iac.hx
+++ src/defold/Iac.hx
@@ -1,10 +1,22 @@
 package defold;
 
+/**
+    Inter-app communication.
+**/
 @:native("_G.iac")
 extern class Iac {
+    /**
+        Set iac listener.
+
+        The listener callback has the following signature: `function(self, payload, type)`
+        where payload is a table with the iac payload.
+    **/
     static function set_listener<T>(listener:T->lua.Table.AnyTable->IacType->Void):Void;
 }
 
+/**
+    Possible IAC types.
+**/
 @:native("_G.iac")
 @:enum extern abstract IacType({}) {
     var TYPE_INVOCATION;
