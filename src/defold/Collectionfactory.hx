1537be38c587fb7c133faa4ef4a99b911ae72429
Dan Korostelev <nadako@gmail.com>
2016-06-09 22:30:30 +0300

    document factories API

--- src/defold/Collectionfactory.hx
+++ src/defold/Collectionfactory.hx
@@ -1,6 +1,26 @@
 package defold;
 
+import defold.Go.GoProperty;
+
+/**
+    Functions for controlling collection factories that are used to dynamically spawn collections.
+**/
 @:native("_G.collectionfactory")
 extern class Collectionfactory {
-    static function create(url:Url, ?position:Vector3, ?rotation:Quaternion, ?properties:lua.Table<Hash,lua.Table.AnyTable>, ?scale:Float):lua.Table<Hash,Hash>;
+    /**
+        Spawn a new instance of a collection into the existing collection.
+
+        The URL identifies the collectionfactory component that should do the spawning.
+
+        Spawning is instant, but spawned game objects get their first update calls the following frame.
+
+        The supplied parameters for position, rotation and scale will be applied to the whole collection when spawned.
+
+        Script properties in the created game objects can be overridden through a properties-parameter table.
+        The table should contain game object ids (hash) as keys and property tables as values to be used when initiating each spawned game object.
+
+        The function returns a table that contains a key for each game object id (hash), as addressed if the collection file was top level,
+        and the corresponding spawned instance id (hash) as value with a unique path prefix added to each instance.
+    **/
+    static function create(url:Url, ?position:Vector3, ?rotation:Quaternion, ?properties:lua.Table<Hash,lua.Table<Hash,GoProperty>>, ?scale:Float):lua.Table<Hash,Hash>;
 }
--- src/defold/Factory.hx
+++ src/defold/Factory.hx
@@ -2,8 +2,20 @@ package defold;
 
 import haxe.extern.EitherType;
 import defold.support.UrlOrString;
+import defold.Go.GoProperty;
 
+/**
+    Functions for controlling factory components that are used to dynamically spawn game objects.
+**/
 @:native("_G.factory")
 extern class Factory {
-    static function create(url:UrlOrString, ?position:Vector3, ?rotation:Quaternion, ?properties:lua.Table<Hash,lua.Table.AnyTable>, ?scale:EitherType<Float,Vector3>):Hash;
+    /**
+        Make a factory create a new game object.
+
+        If the game object is created inside of the frame (e.g. from an update callback), the game object will be created instantly,
+        but none of its component will be updated in the same frame.
+
+        Properties defined in scripts in the created game object can be overridden through the properties-parameter below.
+    **/
+    static function create(url:UrlOrString, ?position:Vector3, ?rotation:Quaternion, ?properties:lua.Table<Hash,GoProperty>, ?scale:EitherType<Float,Vector3>):Hash;
 }
