4460ab8c7d4fd46fae09ce02723466742dec97b1
Dan Korostelev <nadako@gmail.com>
2016-06-09 19:03:18 +0300

    Add some documentation.

--- src/Defold.hx
+++ src/Defold.hx
@@ -1,8 +1,27 @@
 import defold.Hash;
 
+/**
+    Built-in scripting functions.
+**/
 @:native("_G")
 extern class Defold {
+    /**
+        Hashes a string.
+
+        All ids in the engine are represented as hashes,
+        so a string needs to be hashed before it can be compared with an id.
+    **/
     static function hash(s:String):Hash;
+
+    /**
+        Get hex representation of a hash value as a string.
+
+        The returned string is always padded with leading zeros.
+    **/
     static function hash_to_hex(h:Hash):String;
+
+    /**
+        Pretty printing of lua values.
+    **/
     static function pprint(v:Dynamic):String;
 }
