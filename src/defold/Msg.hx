486613635e0dc9b856b346d595cff80e1ab80119
Dan Korostelev <nadako@gmail.com>
2016-06-10 02:10:24 +0300

    Document msg.

--- src/defold/Msg.hx
+++ src/defold/Msg.hx
@@ -4,11 +4,26 @@ import haxe.extern.EitherType;
 import defold.support.HashOrString;
 import defold.support.HashOrStringOrUrl;
 
+/**
+    Functions for passing messages and constructing URL objects.
+**/
 @:native("_G.msg")
 extern class Msg {
+    /**
+        Posts a message to a receiving URL.
+    **/
     @:overload(function(receiver:Null<HashOrStringOrUrl>, message_id:Message<Void>):Void {})
     static function post<T>(receiver:Null<HashOrStringOrUrl>, message_id:Message<T>, message:T):Void;
 
+    /**
+        The format of the string must be "[socket:][path][#fragment]", which is similar to a http URL.
+
+        When addressing instances, `socket` is the name of the collection.
+        `path` is the id of the instance, which can either be relative the instance of the calling script or global.
+        `fragment` would be the id of the desired component.
+
+        Calling without `urlstring` is equivalent to `Msg.url("")`.
+    **/
     @:overload(function():Url {})
     @:overload(function(socket:EitherType<String,Int>, path:HashOrString, fragment:HashOrString):Url {})
     static function url(urlstring:String):Url;
