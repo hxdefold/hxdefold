544ea9a7c0e7a992dca32d0395d4300e7a365c24
Dan Korostelev <nadako@gmail.com>
2016-06-12 21:39:29 +0300

    document http API

--- src/defold/Http.hx
+++ src/defold/Http.hx
@@ -1,16 +1,42 @@
 package defold;
 
+/**
+    Functions for performing HTTP requests.
+**/
 @:native("_G.http")
 extern class Http {
+    /**
+        Perform a HTTP request.
+    **/
     static function request<T>(url:String, method:String, callback:T->Hash->HttpResponse->Void, ?headers:lua.Table<String,String>, ?post_data:String, ?options:HttpOptions):Void;
 }
 
+/**
+    Type for the `response` argument for the http request callback.
+**/
 typedef HttpResponse = {
+    /**
+        Status code.
+    **/
     var status:Int;
+
+    /**
+        Response data.
+    **/
     var response:String;
+
+    /**
+        Response headers.
+    **/
     var headers:haxe.DynamicAccess<String>;
 }
 
+/**
+    Type for the `options` argument of `Http.request` method.
+**/
 typedef HttpOptions = {
-    @:optional var timeout:Int;
+    /**
+        Request timeout in seconds
+    **/
+    @:optional var timeout:Float;
 }
