735a95684ef93690c1703a74bf3503466fce7546
Dan Korostelev <nadako@gmail.com>
2016-06-13 21:36:53 +0300

    hide vector ops

--- src/defold/types/Vector3.hx
+++ src/defold/types/Vector3.hx
@@ -6,17 +6,17 @@ package defold.types;
 @:forward
 extern abstract Vector3(Vector3Data) {
     @:op(a + b)
-    static inline function add(a:Vector3, b:Vector3):Vector3 {
+    private static inline function add(a:Vector3, b:Vector3):Vector3 {
         return untyped __lua__("({0}) + ({1})", a, b);
     }
 
     @:op(a - b)
-    static inline function sub(a:Vector3, b:Vector3):Vector3 {
+    private static inline function sub(a:Vector3, b:Vector3):Vector3 {
         return untyped __lua__("({0}) - ({1})", a, b);
     }
 
     @:op(a * b) @:commutative
-    static inline function mul(a:Vector3, b:Float):Vector3 {
+    private static inline function mul(a:Vector3, b:Float):Vector3 {
         return untyped __lua__("({0}) * ({1})", a, b);
     }
 }
--- src/defold/types/Vector4.hx
+++ src/defold/types/Vector4.hx
@@ -6,17 +6,17 @@ package defold.types;
 @:forward
 extern abstract Vector4(Vector4Data) {
     @:op(a + b)
-    static inline function add(a:Vector4, b:Vector4):Vector4 {
+    private static inline function add(a:Vector4, b:Vector4):Vector4 {
         return untyped __lua__("({0}) + ({1})", a, b);
     }
 
     @:op(a - b)
-    static inline function sub(a:Vector4, b:Vector4):Vector4 {
+    private static inline function sub(a:Vector4, b:Vector4):Vector4 {
         return untyped __lua__("({0}) - ({1})", a, b);
     }
 
     @:op(a * b) @:commutative
-    static inline function mul(a:Vector4, b:Float):Vector4 {
+    private static inline function mul(a:Vector4, b:Float):Vector4 {
         return untyped __lua__("({0}) * ({1})", a, b);
     }
 }
