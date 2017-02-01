aed958c7ed5f8af495a0c606596d30e62d47b594
Dan Korostelev <nadako@gmail.com>
2017-02-02 02:49:47 +0300

    Update builtins module (`Defold` in haxe).

--- src/Defold.hx
+++ src/Defold.hx
@@ -8,8 +8,10 @@ extern class Defold {
     /**
         Hashes a string.
 
-        All ids in the engine are represented as hashes,
-        so a string needs to be hashed before it can be compared with an id.
+        All ids in the engine are represented as hashes, so a string needs to be hashed
+        before it can be compared with an id.
+
+        @param s string to hash
     **/
     static function hash(s:String):Hash;
 
@@ -17,11 +19,15 @@ extern class Defold {
         Get hex representation of a hash value as a string.
 
         The returned string is always padded with leading zeros.
+
+        @param h hash value to get hex string for
     **/
     static function hash_to_hex(h:Hash):String;
 
     /**
         Pretty printing of lua values.
+
+        @param v value to print
     **/
-    static function pprint(v:Dynamic):String;
+    static function pprint(v:Any):String;
 }
