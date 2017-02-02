fe7319e04c27205cfb89eea666fe8ec8e412e96a
Dan Korostelev <nadako@gmail.com>
2017-02-02 04:28:20 +0300

    update collectionfactory

--- src/defold/Collectionfactory.hx
+++ src/defold/Collectionfactory.hx
@@ -4,7 +4,8 @@ import defold.Go.GoProperty;
 import defold.types.*;
 
 /**
-    Functions for controlling collection factories that are used to dynamically spawn collections.
+    Functions for controlling collection factory components which are
+    used to dynamically spawn collections into the runtime.
 **/
 @:native("_G.collectionfactory")
 extern class Collectionfactory {
@@ -13,15 +14,27 @@ extern class Collectionfactory {
 
         The URL identifies the collectionfactory component that should do the spawning.
 
-        Spawning is instant, but spawned game objects get their first update calls the following frame.
+        Spawning is instant, but spawned game objects get their first update calls the following frame. The supplied parameters for position, rotation and scale
+        will be applied to the whole collection when spawned.
 
-        The supplied parameters for position, rotation and scale will be applied to the whole collection when spawned.
+        Script properties in the created game objects can be overridden through
+        a properties-parameter table. The table should contain game object ids
+        (hash) as keys and property tables as values to be used when initiating each
+        spawned game object.
 
-        Script properties in the created game objects can be overridden through a properties-parameter table.
-        The table should contain game object ids (hash) as keys and property tables as values to be used when initiating each spawned game object.
+        See go.property for more information on script properties.
 
-        The function returns a table that contains a key for each game object id (hash), as addressed if the collection file was top level,
-        and the corresponding spawned instance id (hash) as value with a unique path prefix added to each instance.
+        The function returns a table that contains a key for each game object
+        id (hash), as addressed if the collection file was top level, and the
+        corresponding spawned instance id (hash) as value with a unique path
+        prefix added to each instance.
+
+        @param url the collection factory component to be used
+        @param position position to assign to the newly spawned collection
+        @param rotation rotation to assign to the newly spawned collection
+        @param properties table of script properties to propagate to any new game object instances (table)
+        @param scale uniform scaling to apply to the newly spawned collection (must be greater than 0)
+        @return a table mapping the id:s from the collection to the new instance id:s
     **/
     static function create(url:Url, ?position:Vector3, ?rotation:Quaternion, ?properties:lua.Table<Hash,lua.Table<Hash,GoProperty>>, ?scale:Float):lua.Table<Hash,Hash>;
 }
