dbededb6e6bd5859bee364f2f5072e367a2e9855
Dan Korostelev <nadako@gmail.com>
2017-02-02 12:46:45 +0300

    Update http module

--- src/defold/Http.hx
+++ src/defold/Http.hx
@@ -3,12 +3,22 @@ package defold;
 import defold.types.Hash;
 
 /**
-    Functions for performing HTTP requests.
+    Functions for performing HTTP and HTTPS requests.
 **/
 @:native("_G.http")
 extern class Http {
     /**
         Perform a HTTP/HTTPS request.
+
+        If no timeout value is passed, the configuration value "network.http_timeout" is used.
+        If that is not set, the timeout value is 0. (0 == blocks indefinitely)
+
+        @param url target url
+        @param method HTTP/HTTPS method, e.g. GET/PUT/POST/DELETE/...
+        @param callback response callback
+        @param headers optional lua-table with custom headers
+        @param post_data optional data to send
+        @param options optional lua-table with request parameters
     **/
     static function request<T>(url:String, method:String, callback:T->Hash->HttpResponse->Void, ?headers:lua.Table<String,String>, ?post_data:String, ?options:HttpOptions):Void;
 }
