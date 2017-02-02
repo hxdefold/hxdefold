32c852e2a6c67a122a5272000a9dc4ec2982e111
Dan Korostelev <nadako@gmail.com>
2017-02-02 04:26:06 +0300

    rework camera module

--- src/defold/Camera.hx
+++ src/defold/Camera.hx
@@ -3,16 +3,16 @@ package defold;
 import defold.types.Message;
 
 /**
-    Functions to control the camera and focus.
+    Messages to control camera components and camera focus.
 
     This module currently has no functions.
 
-    See `CameraMessages` for camera-related messages.
+    See `CameraMessages` for related messages.
 **/
 extern class Camera {}
 
 /**
-    Messages related to camera components.
+    Messages related to the `Camera` module.
 **/
 @:publicFields
 class CameraMessages {
@@ -22,51 +22,49 @@ class CameraMessages {
         Post this message to a camera-component to activate it.
 
         Several cameras can be active at the same time, but only the camera that was last activated will be used for rendering.
-        When the camera is deactivated (see `ReleaseCameraFocus`), the previously activated camera will again be used for rendering automatically.
+        When the camera is deactivated (see `release_camera_focus`), the previously activated camera will again be used for rendering automatically.
 
-        The reason it is called "camera focus" is the similarity to how acquiring input focus works (see `GoMessages.AcquireInputFocus`).
+        The reason it is called "camera focus" is the similarity to how acquiring input focus works (see `acquire_input_focus`).
     **/
-    static var AcquireCameraFocus(default,never) = new Message<Void>("acquire_camera_focus");
+    static var acquire_camera_focus(default, never) = new Message<Void>("acquire_camera_focus");
 
     /**
         Deactivates the receiving camera.
 
-        Post this message to a camera-component to deactivate it.
-        The camera is then removed from the active cameras.
-
-        See `AcquireCameraFocus` for more information how the active cameras are used in rendering.
+        Post this message to a camera-component to deactivate it. The camera is then removed from the active cameras.
+        See `acquire_camera_focus` for more information how the active cameras are used in rendering.
     **/
-    static var ReleaseCameraFocus(default,never) = new Message<Void>("release_camera_focus");
+    static var release_camera_focus(default, never) = new Message<Void>("release_camera_focus");
 
     /**
         Sets camera properties.
 
         Post this message to a camera-component to set its properties at run-time.
     **/
-    static var SetCamera(default,never) = new Message<CameraMessageSetCamera>("set_camera");
+    static var set_camera(default, never) = new Message<CameraMessageSetCamera>("set_camera");
 }
 
 /**
-    Data for the `CameraMessages.SetCamera` message.
+    Data for the `CameraMessages.set_camera` message.
 **/
 typedef CameraMessageSetCamera = {
     /**
-        Aspect ratio of the screen (width divided by height).
+        Aspect ratio of the screen (width divided by height)
     **/
     var aspect_ratio:Float;
 
     /**
-        Field of view of the lens, measured as the angle between the right and left edge (radians).
+        Field of view of the lens, measured as the angle between the right and left edge (radians)
     **/
     var fov:Float;
 
     /**
-        Position of the near clipping plane (distance from camera along relative z).
+        Position of the near clipping plane (distance from camera along relative z)
     **/
     var near_z:Float;
 
     /**
-        Position of the far clipping plane (distance from camera along relative z).
+        Position of the far clipping plane (distance from camera along relative z)
     **/
     var far_z:Float;
 }
