c75a83e58b7002f18e82071f04b5d2fc140a83b4
Dan Korostelev <nadako@gmail.com>
2017-02-03 00:14:57 +0300

    update iac

--- src/defold/Iac.hx
+++ src/defold/Iac.hx
@@ -1,15 +1,18 @@
 package defold;
 
 /**
-    Inter-app communication.
+    Functions and constants for doing inter-app communication on iOS and
+    Android.
 **/
 @:native("_G.iac")
 extern class Iac {
     /**
         Set iac listener.
 
-        The listener callback has the following signature: `function(self, payload, type)`
-        where payload is a table with the iac payload.
+        The listener callback has the following signature: function(self, payload, type) where payload is a table
+        with the iac payload and type is an iac.TYPE_<TYPE> enumeration.
+
+        @param listener listener callback function
     **/
     static function set_listener<T>(listener:T->lua.Table.AnyTable->IacType->Void):Void;
 }
