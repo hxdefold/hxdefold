2a0fca32d616e6132e3183788123ac4b32622e49
Dan Korostelev <nadako@gmail.com>
2017-02-03 02:44:53 +0300

    Update json module, add label module.

--- src/defold/Json.hx
+++ src/defold/Json.hx
@@ -1,12 +1,16 @@
 package defold;
 
 /**
-    Decoding of JSON data strings.
+    Manipulation of JSON data strings.
 **/
 @:native("_G.json")
 extern class Json {
     /**
         Decode a string of JSON data into a Lua table.
+        A Lua error is raised for syntax errors.
+
+        @param json json data
+        @return decoded json
     **/
     static function decode(json:String):lua.Table<String,Dynamic>;
 }
---
+++ src/defold/Label.hx
@@ -0,0 +1,20 @@
+package defold;
+
+import defold.types.HashOrString;
+import defold.types.UrlOrString;
+
+/**
+    Functions to manipulate a label component.
+
+    See `LabelProperties` for related properties.
+**/
+@:native("_G.label")
+extern class Label {
+    /**
+        Set the text for a label.
+
+        @param url the label that should have a constant set
+        @param text the text
+    **/
+    static function set_text(url:UrlOrString, text:HashOrString):Void;
+}
