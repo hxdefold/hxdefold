dae0afd9d93d7e8173828c916dbc739ae5664bd9
Dan Korostelev <nadako@gmail.com>
2017-02-03 02:41:53 +0300

    Update image module

--- src/defold/Image.hx
+++ src/defold/Image.hx
@@ -7,6 +7,10 @@ package defold;
 extern class Image {
     /**
         Load image (PNG or JPEG) from buffer.
+
+        @param buffer image data buffer
+        @param premult premultiply alpha. optional and defaults to false
+        @return object with the following fields: width, height, type and buffer (raw data). nil is returned if loading fails.
     **/
     static function load(buffer:String, ?premult:Bool):Null<ImageLoadResult>;
 }
@@ -25,7 +29,7 @@ typedef ImageLoadResult = {
     Image type, used in `ImageLoadResult.type`.
 **/
 @:native("_G.image")
-@:enum extern abstract ImageType({}) {
+@:enum extern abstract ImageType(Int) {
     var TYPE_LUMINANCE;
     var TYPE_RGB;
     var TYPE_RGBA;
