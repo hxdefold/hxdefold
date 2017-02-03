7e153751f06ec049e71fc253bdcc2ff3356a4a99
Dan Korostelev <nadako@gmail.com>
2017-02-03 04:34:43 +0300

    update zlib

--- src/defold/Zlib.hx
+++ src/defold/Zlib.hx
@@ -1,17 +1,27 @@
 package defold;
 
 /**
-    Functions for compression and decompression.
+    Functions for compression and decompression of string buffers.
 **/
 @:native("_G.zlib")
 extern class Zlib {
     /**
         Deflate (compress) a buffer.
+
+        A lua error is raised is on error
+
+        @param buf buffer to deflate
+        @return deflated buffer
     **/
     static function deflate(buf:String):String;
 
     /**
         Inflate (decompress) a buffer.
+
+        A lua error is raised is on error
+
+        @param buf buffer to inflate
+        @return inflated buffer
     **/
     static function inflate(buf:String):String;
 }
