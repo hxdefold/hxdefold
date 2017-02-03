3c07a914cde893c810ab430882f69f38701bd8e1
Dan Korostelev <nadako@gmail.com>
2017-02-03 04:33:47 +0300

    Update Window

--- src/defold/Window.hx
+++ src/defold/Window.hx
@@ -1,22 +1,34 @@
 package defold;
 
 /**
-    Getting access to the window, window event listeners and screen dimming.
+    Functions and constants to access the window, window event listeners
+    and screen dimming.
 **/
 @:native("_G.window")
 extern class Window {
     /**
-        Get the mode for screen dimming
+        Get the mode for screen dimming.
+
+        The dimming mode specifies whether or not a mobile device should dim the screen after a period without user interaction.
+
+        @return mode The mode for screen dimming
     **/
     static function get_dim_mode():WindowDimmingMode;
 
     /**
-        Set the mode for screen dimming
+        Set the mode for screen dimming.
+
+        The dimming mode specifies whether or not a mobile device should dim the screen after a period without user interaction.
+        The dimming mode will only affect the mobile device while the game is in focus on the device, but not when the game is running in the background.
+
+        @param mode The mode for screen dimming
     **/
     static function set_dim_mode(mode:WindowDimmingMode):Void;
 
     /**
-        Sets a window event listener
+        Sets a window event listener.
+
+        @param callback A callback which receives info about window events. Can be null.
     **/
     static function set_listener<T>(callback:T->WindowEvent->WindowEventData->Void):Void;
 }
