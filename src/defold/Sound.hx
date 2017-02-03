01795954aa4be035676fe42db143d3c49275837b
Dan Korostelev <nadako@gmail.com>
2017-02-03 03:24:22 +0300

    Update sound module

--- src/defold/Sound.hx
+++ src/defold/Sound.hx
@@ -3,16 +3,20 @@ package defold;
 import defold.types.*;
 
 /**
-    Functions and messages for controlling sound components.
+    Functions and messages for controlling sound components and
+    mixer groups.
 
-    See `SoundMessages` for sound component messages.
+    See `SoundMessages` for related messages.
 **/
 @:native("_G.sound")
 extern class Sound {
     /**
-        Get mixer group gain.
+        Get mixer group gain
 
         Note that gain is in linear scale.
+
+        @param group group name
+        @return gain in linear scale
     **/
     static function get_group_gain(group:HashOrString):Float;
 
@@ -20,75 +24,96 @@ extern class Sound {
         Get a mixer group name as a string.
 
         Note that this function does not return correct group name in release mode
+
+        @param group group name
+        @return group name
     **/
     static function get_group_name(group:HashOrString):String;
 
     /**
-        Get all mixer group names.
+        Get all mixer group names
+
+        @return table of mixer groups names
     **/
     static function get_groups():lua.Table<Int,Hash>;
 
     /**
-        Get peak gain value from mixer group.
+        Get peak value from mixer group.
 
-        Note that the returned value might be an approximation and
-        in particular the effective window might be larger than specified.
+        Note that the returned value might be an approximation and in particular
+        the effective window might be larger than specified.
+
+        @param group group name
+        @param window window length in seconds
     **/
     static function get_peak(group:HashOrString, window:Float):SoundLeftRight<Float>;
 
     /**
         Get RMS (Root Mean Square) value from mixer group.
 
-        Note that the returned value might be an approximation and
-        in particular the effective window might be larger than specified.
+        Note that the returned value might be an approximation and in particular
+        the effective window might be larger than specified.
+
+        @param group group name
+        @param window window length in seconds
     **/
     static function get_rms(group:HashOrString, window:Float):SoundLeftRight<Float>;
 
     /**
-        Checks if background music is playing, e.g. from iTunesю
+        Checks if background music is playing, e.g. from iTunes
+
+        @return true if music is playing
     **/
     static function is_music_playing():Bool;
 
     /**
-        Set mixer group gain.
+        Checks if a phone call is active. If there is an active phone call all
+        other sounds will be muted until the phone call is finished.
+
+        @return true if there is an active phone call
+    **/
+    static function is_phone_call_active():Bool;
+
+    /**
+        Set mixer group gain
 
         Note that gain is in linear scale.
+
+        @param group group name
+        @param gain gain in linear scale
     **/
     static function set_group_gain(group:HashOrString, gain:Float):Bool;
 }
 
 /**
-    Messages related to sound components.
+    Messages related to the `Sound` module.
 **/
 @:publicFields
 class SoundMessages {
     /**
         Plays a sound.
 
-        Post this message to a sound-component to make it play its sound.
-        Multiple voices is supported. The limit is set to 32 voices per sound component.
+        Post this message to a sound-component to make it play its sound. Multiple voices is support. The limit is set to 32 voices per sound component.
     **/
-    static var PlaySound(default,never) = new Message<SoundMessagePlaySound>("play_sound");
+    static var play_sound(default, never) = new Message<SoundMessagePlaySound>("play_sound");
 
     /**
         Set sound gain.
 
         Post this message to a sound-component to set gain on all active playing voices.
-
-        `gain` is between 0 and 1, default is 1.
     **/
-    static var SetGain(default,never) = new Message<{?gain:Float}>("set_gain");
+    static var set_gain(default, never) = new Message<SoundMessageSetGain>("set_gain");
 
     /**
         Stop a playing a sound(s).
 
-        Post this message to a sound-component to make it stop playing all active voices.
+        Post this message to a sound-component to make it stop playing all active voices
     **/
-    static var StopSound(default,never) = new Message<Void>("stop_sound");
+    static var stop_sound(default, never) = new Message<Void>("stop_sound");
 }
 
 /**
-    Data for `SoundMessages.PlaySound` message.
+    Data for the `SoundMessages.play_sound` message.
 **/
 typedef SoundMessagePlaySound = {
     /**
@@ -96,6 +121,16 @@ typedef SoundMessagePlaySound = {
     **/
     @:optional var delay:Float;
 
+    /**
+        Sound gain between 0 and 1, default is 1.
+    **/
+    @:optional var gain:Float;
+}
+
+/**
+    Data for the `SoundMessages.set_gain` message.
+**/
+typedef SoundMessageSetGain = {
     /**
         Sound gain between 0 and 1, default is 1.
     **/
