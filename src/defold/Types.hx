4703bde84934546c538bab985582607a3bc3faae
haath <haath@protonmail.com>
2025-10-17 14:22:29 +0200

    Update to Defold API 1.9.3

---
+++ src/defold/Types.hx
@@ -0,0 +1,28 @@
+package defold;
+
+/**
+ * Functions for checking Defold userdata types.
+ */
+@:native("_G.types")
+extern final class Types {
+	/** Check if passed value is a hash */
+	@:native('is_hash') static function isHash(v:Any):Bool;
+
+	/** Check if passed value is matrix4 */
+	@:native('is_matrix4') static function isMatrix4(v:Any):Bool;
+
+	/** Check if passed value is quaternion */
+	@:native('is_quat') static function isQuat(v:Any):Bool;
+
+	/** Check if passed value is URL */
+	@:native('is_url') static function isUrl(v:Any):Bool;
+
+	/** Check if passed value is vector */
+	@:native('is_vector') static function isVector(v:Any):Bool;
+
+	/** Check if passed value is vector3 */
+	@:native('is_vector3') static function isVector3(v:Any):Bool;
+
+	/** Check if passed value is vector4 */
+	@:native('is_vector4') static function isVector4(v:Any):Bool;
+}
