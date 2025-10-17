ded595dfa23800e427a81df6bde052c21cf2f684
haath <haath@protonmail.com>
2025-10-17 15:09:18 +0200

    Update to Defold API 1.10.1

--- src/defold/Profiler.hx
+++ src/defold/Profiler.hx
@@ -1,181 +1,185 @@
 package defold;
 
 /**
-    Functions for getting profiling data in runtime.
-    More detailed <a href="https://www.defold.com/manuals/profiling/">profiling</a> and <a href="http://www.defold.com/manuals/debugging/">debugging</a> information available in the manuals.</p>
- **/
+	Functions for getting profiling data in runtime.
+	More detailed <a href="https://www.defold.com/manuals/profiling/">profiling</a> and <a href="http://www.defold.com/manuals/debugging/">debugging</a> information available in the manuals.</p>
+**/
 @:native("_G.profiler")
-extern final class Profiler
-{
-    /**
-        Creates and shows or hides and destroys the on-sceen profiler ui
+extern final class Profiler {
+	/**
+		Creates and shows or hides and destroys the on-sceen profiler ui
 
-        The profiler is a real-time tool that shows the numbers of milliseconds spent
-        in each scope per frame as well as counters. The profiler is very useful for
-        tracking down performance and resource problems.
+		The profiler is a real-time tool that shows the numbers of milliseconds spent
+		in each scope per frame as well as counters. The profiler is very useful for
+		tracking down performance and resource problems.
 
-        @param enabled true to enable, false to disable
-    **/
-    @:native('enable_ui')
-    static function enableUi(enabled:Bool):Void;
+		@param enabled true to enable, false to disable
+	**/
+	@:native('enable_ui')
+	static function enableUi(enabled:Bool):Void;
 
-    /**
-        Get the percent of CPU usage by the application, as reported by the OS.
+	/**
+		Get the percent of CPU usage by the application, as reported by the OS.
 
-        This function is not available on HTML5.
+		This function is not available on HTML5.
 
-        For some platforms (Android, Linux and Windows), this information is only available
-        by default in the debug version of the engine. It can be enabled in release version as well
-        by checking `track_cpu` under `profiler` in the `game.project` file.
-        (This means that the engine will sample the CPU usage in intervalls during execution even in release mode.)
+		For some platforms (Android, Linux and Windows), this information is only available
+		by default in the debug version of the engine. It can be enabled in release version as well
+		by checking `track_cpu` under `profiler` in the `game.project` file.
+		(This means that the engine will sample the CPU usage in intervalls during execution even in release mode.)
 
-        @return number of CPU used by the application
-    **/
-    @:native('get_cpu_usage')
-    static function getCpuUsage():Float;
+		@return number of CPU used by the application
+	**/
+	@:native('get_cpu_usage')
+	static function getCpuUsage():Float;
 
-    /**
-        Get the amount of memory used (resident/working set) by the application in bytes, as reported by the OS.
+	/**
+		Get the amount of memory used (resident/working set) by the application in bytes, as reported by the OS.
 
-        This function is not available on HTML5.
+		This function is not available on HTML5.
 
-        The values are gathered from internal OS functions which correspond to the following;
+		The values are gathered from internal OS functions which correspond to the following;
 
-        OS                          | Value
-        ----------------------------|------------------
-        iOS, OSX, Android and Linux | Resident memory
-        Windows                     | Working set
-        HTML5                       | Not available
+		OS                          | Value
+		----------------------------|------------------
+		iOS, OSX, Android and Linux | Resident memory
+		Windows                     | Working set
+		HTML5                       | Not available
 
-        @return bytes used by the application
-    **/
-    @:native('get_memory_usage')
-    static function getMemoryUsage():Int;
+		@return bytes used by the application
+	**/
+	@:native('get_memory_usage')
+	static function getMemoryUsage():Int;
 
-    /**
-        Get the number of recorded frames in the on-screen profiler ui recording buffer
+	/**
+		Get the number of recorded frames in the on-screen profiler ui recording buffer
 
-        @return the number of recorded frames, zero if on-screen profiler is disabled
-    **/
-    @:native('recorded_frame_count')
-    static function recordedFrameCount():Int;
+		@return the number of recorded frames, zero if on-screen profiler is disabled
+	**/
+	@:native('recorded_frame_count')
+	static function recordedFrameCount():Int;
 
-    /**
-        Set the on-screen profile mode - run, pause, record or show peak frame
+	/**
+		Set the on-screen profile mode - run, pause, record or show peak frame
 
-        @param mode the mode to set the ui profiler in
+		@param mode the mode to set the ui profiler in
 
-        To stop recording, switch to a different mode such as `MODE_PAUSE` or `MODE_RUN`.
-        You can also use the `view_recorded_frame` function to display a recorded frame. Doing so stops the recording as well.
+		To stop recording, switch to a different mode such as `MODE_PAUSE` or `MODE_RUN`.
+		You can also use the `view_recorded_frame` function to display a recorded frame. Doing so stops the recording as well.
 
-        Every time you switch to recording mode the recording buffer is cleared.
-        The recording buffer is also cleared when setting the `MODE_SHOW_PEAK_FRAME` mode.
-    **/
-    @:native('set_ui_mode')
-    static function setUiMode(mode:ProfilerMode):Void;
+		Every time you switch to recording mode the recording buffer is cleared.
+		The recording buffer is also cleared when setting the `MODE_SHOW_PEAK_FRAME` mode.
+	**/
+	@:native('set_ui_mode')
+	static function setUiMode(mode:ProfilerMode):Void;
 
-    /**
-        Set the on-screen profile view mode - minimized or expanded
+	/**
+		Set the on-screen profile view mode - minimized or expanded
 
-        @param mode the view mode to set the ui profiler in
-    **/
-    @:native('set_ui_view_mode')
-    static function setUiViewMode(mode:ProfilerViewMode):Void;
+		@param mode the view mode to set the ui profiler in
+	**/
+	@:native('set_ui_view_mode')
+	static function setUiViewMode(mode:ProfilerViewMode):Void;
 
-    /**
-        Shows or hides the time the engine waits for vsync in the on-screen profiler
+	/**
+		Shows or hides the time the engine waits for vsync in the on-screen profiler
 
-        Each frame the engine waits for vsync and depending on your vsync settings and how much time
-        your game logic takes this time can dwarf the time in the game logic making it hard to
-        see details in the on-screen profiler graph and lists.
+		Each frame the engine waits for vsync and depending on your vsync settings and how much time
+		your game logic takes this time can dwarf the time in the game logic making it hard to
+		see details in the on-screen profiler graph and lists.
 
-        Also, by hiding this the FPS times in the header show the time spent each time excuding the
-        time spent waiting for vsync. This shows you how long time your game is spending actively
-        working each frame.
+		Also, by hiding this the FPS times in the header show the time spent each time excuding the
+		time spent waiting for vsync. This shows you how long time your game is spending actively
+		working each frame.
 
-        This setting also effects the display of recorded frames but does not affect the actual
-        recorded frames so it is possible to toggle this on and off when viewing recorded frames.
+		This setting also effects the display of recorded frames but does not affect the actual
+		recorded frames so it is possible to toggle this on and off when viewing recorded frames.
 
-        By default the vsync wait times is displayed in the profiler.
+		By default the vsync wait times is displayed in the profiler.
 
-        @param visible true to include it in the display, false to hide it.
-    **/
-    @:native('set_ui_vsync_wait_visible')
-    static function setUiVsyncWaitVisible(visible:Bool):Void;
+		@param visible true to include it in the display, false to hide it.
+	**/
+	@:native('set_ui_vsync_wait_visible')
+	static function setUiVsyncWaitVisible(visible:Bool):Void;
 
-    /**
-        Displays a previously recorded frame in the on-screen profiler ui.
+	/**
+		Displays a previously recorded frame in the on-screen profiler ui.
 
-        Pauses and displays a frame from the recording buffer in the on-screen profiler ui
+		Pauses and displays a frame from the recording buffer in the on-screen profiler ui
 
-        The frame to show can either be an absolute frame or a relative frame to the current frame.
-    **/
-    @:native('view_recorded_frame')
-    static function viewRecordedFrame(frame_index:ProfilerViewRecordedFrame):Void;
+		The frame to show can either be an absolute frame or a relative frame to the current frame.
+	**/
+	@:native('view_recorded_frame')
+	static function viewRecordedFrame(frame_index:ProfilerViewRecordedFrame):Void;
+
+	/**
+		Dump the current frame profiling data to the output.
+
+		Useful for capturing a snapshot of timing/scopes for the frame.
+	**/
+	@:native('dump_frame')
+	static function dumpFrame():Void;
 }
 
 /**
-    Data for `Profiler.view_recorded_frame`.
+	Data for `Profiler.view_recorded_frame`.
 **/
-typedef ProfilerViewRecordedFrame =
-{
-    /**
-        The offset from the currently displayed frame (this is truncated between zero and the number of recorded frames)
-    **/
-    var distance:Int;
+typedef ProfilerViewRecordedFrame = {
+	/**
+		The offset from the currently displayed frame (this is truncated between zero and the number of recorded frames)
+	**/
+	var distance:Int;
 
-    /**
-        The frame index in the recording buffer (1 is first recorded frame)
-    **/
-    var frame:Int;
+	/**
+		The frame index in the recording buffer (1 is first recorded frame)
+	**/
+	var frame:Int;
 }
 
 /**
-    Possible values for `Profiler.set_ui_mode`.
+	Possible values for `Profiler.set_ui_mode`.
 **/
 @:native("_G.profiler")
-extern enum abstract ProfilerMode({})
-{
-    /**
-        Pauses on the currently displayed frame
-    **/
-    @:native('MODE_PAUSE')
-    var Pause;
+extern enum abstract ProfilerMode({}) {
+	/**
+		Pauses on the currently displayed frame
+	**/
+	@:native('MODE_PAUSE')
+	var Pause;
 
-    /**
-        Records all incoming frames to the recording buffer
-    **/
-    @:native('MODE_RECORD')
-    var Record;
+	/**
+		Records all incoming frames to the recording buffer
+	**/
+	@:native('MODE_RECORD')
+	var Record;
 
-    /**
-        This is default mode that continously shows the last frame
-    **/
-    @:native('MODE_RUN')
-    var run;
+	/**
+		This is default mode that continously shows the last frame
+	**/
+	@:native('MODE_RUN')
+	var run;
 
-    /**
-        Pauses on the currently displayed frame but shows a new frame if that frame is slower
-    **/
-    @:native('MODE_SHOW_PEAK_FRAME')
-    var ShowPeakFrame;
+	/**
+		Pauses on the currently displayed frame but shows a new frame if that frame is slower
+	**/
+	@:native('MODE_SHOW_PEAK_FRAME')
+	var ShowPeakFrame;
 }
 
 /**
-    Possible values for `Profiler.set_ui_view_mode`.
+	Possible values for `Profiler.set_ui_view_mode`.
 **/
 @:native("_G.profiler")
-extern enum abstract ProfilerViewMode({})
-{
-    /**
-        The default mode which displays all the ui profiler details
-    **/
-    @:native('VIEW_MODE_FULL')
-    var ModeFull;
+extern enum abstract ProfilerViewMode({}) {
+	/**
+		The default mode which displays all the ui profiler details
+	**/
+	@:native('VIEW_MODE_FULL')
+	var ModeFull;
 
-    /**
-        Minimized mode which only shows the top header (fps counters and ui profiler mode)
-    **/
-    @:native('VIEW_MODE_MINIMIZED')
-    var Minimized;
+	/**
+		Minimized mode which only shows the top header (fps counters and ui profiler mode)
+	**/
+	@:native('VIEW_MODE_MINIMIZED')
+	var Minimized;
 }
--- src/defold/support/ScriptOnInputAction.hx
+++ src/defold/support/ScriptOnInputAction.hx
@@ -3,190 +3,188 @@ package defold.support;
 import defold.types.util.LuaArray;
 
 /**
-    Type of the `action` argument of the `Script.onInput` method.
+	Type of the `action` argument of the `Script.onInput` method.
 **/
-extern final class ScriptOnInputAction
-{
-    /**
-        The amount of input given by the user.
-        This is usually 1 for buttons and 0-1 for analogue inputs.
-        This is not present for mouse movement.
-    **/
-    var value:Float;
+extern final class ScriptOnInputAction {
+	/**
+		The amount of input given by the user.
+		This is usually 1 for buttons and 0-1 for analogue inputs.
+		This is not present for mouse movement.
+	**/
+	var ?value:Float;
 
-    /**
-        If the input was pressed this frame.
-        This is not present for mouse movement.
-    **/
-    var pressed:Bool;
+	/**
+		If the input was pressed this frame.
+		This is not present for mouse movement.
+	**/
+	var ?pressed:Bool;
 
-    /**
-        If the input was released this frame.
-        This is not present for mouse movement.
-    **/
-    var released:Bool;
+	/**
+		If the input was released this frame.
+		This is not present for mouse movement.
+	**/
+	var ?released:Bool;
 
-    /**
-        If the input was repeated this frame.
-        This is similar to how a key on a keyboard is repeated when you hold it down.
-        This is not present for mouse movement.
-    **/
-    var repeated:Bool;
+	/**
+		If the input was repeated this frame.
+		This is similar to how a key on a keyboard is repeated when you hold it down.
+		This is not present for mouse movement.
+	**/
+	var ?repeated:Bool;
 
-    /**
-        The x value of a pointer device, if present.
-    **/
-    var x:Float;
+	/**
+		The x value of a pointer device, if present.
+	**/
+	var ?x:Float;
 
-    /**
-        The y value of a pointer device, if present.
-    **/
-    var y:Float;
+	/**
+		The y value of a pointer device, if present.
+	**/
+	var ?y:Float;
 
-    /**
-        The screen space x value of a pointer device, if present.
-    **/
-    @:native('screen_x')
-    var screenX:Float;
+	/**
+		The screen space x value of a pointer device, if present.
+	**/
+	@:native('screen_x')
+	var ?screenX:Float;
 
-    /**
-        The screen space y value of a pointer device, if present.
-    **/
-    @:native('screen_y')
-    var screenY:Float;
+	/**
+		The screen space y value of a pointer device, if present.
+	**/
+	@:native('screen_y')
+	var ?screenY:Float;
 
-    /**
-        The change in x value of a pointer device, if present.
-    **/
-    var dx:Float;
+	/**
+		The change in x value of a pointer device, if present.
+	**/
+	var ?dx:Float;
 
-    /**
-        The change in y value of a pointer device, if present.
-    **/
-    var dy:Float;
+	/**
+		The change in y value of a pointer device, if present.
+	**/
+	var ?dy:Float;
 
-    /**
-        The change in screen space x value of a pointer device, if present.
-    **/
-    @:native('screen_dx')
-    var screenDx:Float;
+	/**
+		The change in screen space x value of a pointer device, if present.
+	**/
+	@:native('screen_dx')
+	var ?screenDx:Float;
 
-    /**
-        The change in screen space y value of a pointer device, if present.
-    **/
-    @:native('screen_dy')
-    var screenDy:Float;
+	/**
+		The change in screen space y value of a pointer device, if present.
+	**/
+	@:native('screen_dy')
+	var ?screenDy:Float;
 
-    /**
-        List of touch input, one element per finger, if present.
-    **/
-    var touch:LuaArray<ScriptOnInputActionTouch>;
+	/**
+		List of touch input, one element per finger, if present.
+	**/
+	var ?touch:LuaArray<ScriptOnInputActionTouch>;
 
-    /**
-        The index of the gamepad device that provided the input.
-    **/
-    var gamepad:Int;
+	/**
+		The index of the gamepad device that provided the input.
+	**/
+	var ?gamepad:Int;
 
-    /**
-        The name of the gamepad that was connected.
-        Available on the `gamepad_connected` input event.
-    **/
-    @:native('gamepad_name')
-    var gamepadName:String;
+	/**
+		The name of the gamepad that was connected.
+		Available on the `gamepad_connected` input event.
+	**/
+	@:native('gamepad_name')
+	var ?gamepadName:String;
 
-    /**
-        Indicates that the connected gamepad is unidentified and will only generate raw input.
-        Available on the `gamepad_connected` input event.
-    **/
-    @:native('gamepad_unknown')
-    var gamepadUnknown:Bool;
+	/**
+		Indicates that the connected gamepad is unidentified and will only generate raw input.
+		Available on the `gamepad_connected` input event.
+	**/
+	@:native('gamepad_unknown')
+	var ?gamepadUnknown:Bool;
 }
 
 /**
-    Type of the `ScriptOnInputAction.touch` field.
+	Type of the `ScriptOnInputAction.touch` field.
 **/
-extern final class ScriptOnInputActionTouch
-{
-    /**
-        A number identifying the touch input during its duration.
-    **/
-    var id:Int;
+extern final class ScriptOnInputActionTouch {
+	/**
+		A number identifying the touch input during its duration.
+	**/
+	var id:Int;
 
-    /**
-        True if the finger was pressed this frame.
-    **/
-    var pressed:Bool;
+	/**
+		True if the finger was pressed this frame.
+	**/
+	var pressed:Bool;
 
-    /**
-        True if the finger was released this frame.
-    **/
-    var released:Bool;
+	/**
+		True if the finger was released this frame.
+	**/
+	var released:Bool;
 
-    /**
-        Number of taps, one for single, two for double-tap, etc.
-    **/
-    @:native('tap_count')
-    var tapCount:Int;
+	/**
+		Number of taps, one for single, two for double-tap, etc.
+	**/
+	@:native('tap_count')
+	var tapCount:Int;
 
-    /**
-        The x touch location.
-    **/
-    var x:Float;
+	/**
+		The x touch location.
+	**/
+	var x:Float;
 
-    /**
-        The y touch location.
-    **/
-    var y:Float;
+	/**
+		The y touch location.
+	**/
+	var y:Float;
 
-    /**
-        The screen space x value of a touch.
-    **/
-    @:native('screen_x')
-    var screenX:Float;
+	/**
+		The screen space x value of a touch.
+	**/
+	@:native('screen_x')
+	var screenX:Float;
 
-    /**
-        The screen space y value of a touch.
-    **/
-    @:native('screen_y')
-    var screenY:Float;
+	/**
+		The screen space y value of a touch.
+	**/
+	@:native('screen_y')
+	var screenY:Float;
 
-    /**
-        The change in x value.
-    **/
-    var dx:Float;
+	/**
+		The change in x value.
+	**/
+	var dx:Float;
 
-    /**
-        The change in y value.
-    **/
-    var dy:Float;
+	/**
+		The change in y value.
+	**/
+	var dy:Float;
 
-    /**
-        The change in screen space x value of a touch.
-    **/
-    @:native('screen_dx')
-    var screenDx:Float;
+	/**
+		The change in screen space x value of a touch.
+	**/
+	@:native('screen_dx')
+	var screenDx:Float;
 
-    /**
-        The change in screen space y value of a touch.
-    **/
-    @:native('screen_dy')
-    var screenDy:Float;
+	/**
+		The change in screen space y value of a touch.
+	**/
+	@:native('screen_dy')
+	var screenDy:Float;
 
-    /**
-        Accelerometer x value (if present).
-    **/
-    @:native('acc_x')
-    var accX:Float;
+	/**
+		Accelerometer x value (if present).
+	**/
+	@:native('acc_x')
+	var accX:Float;
 
-    /**
-        Accelerometer y value (if present).
-    **/
-    @:native('acc_y')
-    var accY:Float;
+	/**
+		Accelerometer y value (if present).
+	**/
+	@:native('acc_y')
+	var accY:Float;
 
-    /**
-        Accelerometer z value (if present).
-    **/
-    @:native('acc_z')
-    var accZ:Float;
+	/**
+		Accelerometer z value (if present).
+	**/
+	@:native('acc_z')
+	var accZ:Float;
 }
